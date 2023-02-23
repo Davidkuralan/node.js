cb32e9ae4bfb7f27a174a7a31529005b5dc1a1c3
Davidkuralan <thiruandshanker@gmail.com>
2023-02-23 20:22:28 +0530

    req and res ...

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const http = require('http');
 const server = http.createServer((req,res) =>{
-    console.log('thiru');
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My Page</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 server.listen(4000);
