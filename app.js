685578add561d7be0e3526226aedaa354901ea46
Davidkuralan <thiruandshanker@gmail.com>
2023-02-26 11:39:02 +0530

    created message file.

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+const { text } = require('stream/consumers');
 const server = http.createServer((req,res) =>{
-    console.log(req.url,req.method,req.headers);
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>');
+        res.write('</html>');
+      return  res.end();        
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data',(chunk) =>{
+            body.push(chunk);
+        });
+        return req.on('end',() =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message, (err) =>{
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
+        });
+       
+    }
     res.setHeader('content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>My Page</title></head>');
